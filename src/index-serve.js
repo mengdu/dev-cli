28b3764f456c15d58ba3ef08ea179b28c50f9e33
mengdu <1752295326@qq.com>
2019-01-16 23:26:08 +0800

    feat: 改用 browser-sync 库实现服务器

--- src/index-serve.js
+++ src/index-serve.js
@@ -2,17 +2,19 @@
 'use strict'
 import path from 'path'
 import program from 'commander'
+import browserSync from 'browser-sync'
 
 program
   .name('dev serve')
   .usage('<dir> [port]')
   .option('-p, --port <port>', 'http server port', Number, 8080)
   .option('-d, --dir [dir]', 'static file dir', '.')
-  .option('-o, --open', 'Open browser automatically', true)
+  .option('-o, --open', 'Open browser automatically')
+  .option('-w, --watch', 'Watch files change', Boolean)
+  .option('-c, --config <config>', 'Use `Browsersync` config file, see: https://browsersync.io/docs/options', null)
 
 program.parse(process.argv)
 
-// console.log(program.port, program.dir, program.args)
 if (!program.port || !program.dir) {
   // 如果输入参数为空
   if (program.args.length < 1) program.help()
@@ -20,6 +22,28 @@ if (!program.port || !program.dir) {
 
 const port = Number(program.args[1]) || program.port
 const dir = path.resolve(process.cwd(), program.args[0] || program.dir)
-const open = program.open
+const open = !!program.open
 
-require('./modules/http-server').default.start({port, dir, open})
+let options = {
+  server: dir,
+  open: open,
+  port: port
+}
+
+if (program.watch) {
+  options.files = dir
+}
+
+if (program.config) {
+  const config = require(path.resolve(dir, program.config))
+
+  if (config && typeof config === 'object') {
+    
+    options = {
+      ...options,
+      ...config
+    }
+  }
+}
+// console.log(options)
+browserSync(options)
