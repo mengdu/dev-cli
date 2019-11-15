423a26c246a472f9953dff00fa3e5fb596e3e72e
ygl <lanyueos@qq.com>
2019-11-15 11:55:56 +0800

    build

--- dest/index.js
+++ dest/index.js
@@ -14,9 +14,7 @@ var _package2 = _interopRequireDefault(_package);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_commander2.default.name('dev').version(_package2.default.version).usage('<command> [options]').option('-i, --info', 'Tool introduction').command('serve', 'Create a local static file server').command('fanyi', 'Translate tool');
-
-_commander2.default.parse(process.argv);
+_commander2.default.name('dev').version(_package2.default.version).description('A devlopment tool').usage('<command> [options]').option('-i, --info', 'Tool introduction').command('serve', 'Create a local static file server').alias('s').command('fanyi', 'Translate tool').alias('f').parse(process.argv);
 
 var version = (0, _utils.color)(' ' + _package2.default.name + ' ', '1;100') + (0, _utils.color)(' v' + _package2.default.version + ' ', '1;104');
 var infos = ['\n  ' + version + ' ' + _package2.default.description + '\n', '     +--------------------------------------------------------+', '     |                                                        |', '     |   npm i -g ' + _package2.default.repository.url + '   |', '     |                                                        |', '     +--------------------------------------------------------+\n', '  + ' + (0, _utils.color)(_package2.default.homepage, 34), '  + Make by ' + _package2.default.author.name + ' <' + (0, _utils.color)(_package2.default.author.email, 32) + '>'];
